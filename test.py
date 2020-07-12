464c83f08184be847734f9ba38ea534bb94fca53
Vlad <vlad.voinea@allcloud.io>
2020-07-12 09:02:02 +0300

    integrated the time splitter

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-from datetime import time
+from datetime import time, datetime
 import json
 
 weekdays = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
@@ -7,12 +7,22 @@ input2 = "Mon-Thu, Sun 11:30 am - 10 pm  / Fri-Sat 11:30 am - 11 pm"
 
 def time_splitter(interval):
     intervals = interval.split("-")
+    opening = intervals[0].strip()
+    closing = intervals[1].strip()
+    try:
+        opening_datetine = datetime.strptime(opening, "%I:%M %p" )
+    except ValueError:
+        opening_datetine = datetime.strptime(opening, "%I %p" )
 
+    try:
+        closing_datetime = datetime.strptime(closing, "%I:%M %p" )
+    except ValueError:
+        closing_datetime = datetime.strptime(closing, "%I %p" )
 
 
     return {
-            "open": str(time(8, 00)),
-            "close": str(time(22, 00))
+            "open": opening_datetine.time(),
+            "close": closing_datetime.time()
         }
 
 def day_splitter(days):
@@ -47,7 +57,10 @@ def unit_splitter(the_unit):
             timetable.update( { day : time_splitter(interval) } )
 
     return timetable
+
+
+    
 #rez = unit_splitter(input2)
 #print(json.dumps(rez, indent=4  ) )
 
-print(time_splitter("11 am - 10:30 pm"))
+print(time_splitter("11 am-10:30 pm"))
